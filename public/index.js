816fb29818c695f69dad6f7464f195cde6a37701
guyinjeans <jdbroberg72@gmail.com>
2017-11-02 12:30:45 -0700

    Added view component

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
+import Conway from './Components/Conway.js';
+import View from './Components/View.js';
 
+//const simulation = new Conway();
+//simulation.run();
+
+const ctx = document.getElementById('renderer').getContext('2d');
+ctx.canvas.width = window.screen.width;
+ctx.canvas.height = window.screen.height;
+const v = new View(ctx);
+v.render();
